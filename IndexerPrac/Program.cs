19f6710de24aa458b8c76926e8660b5353bb7928
JUDY <s2018s05@e-mirim.hs.kr>
2020-06-26 09:55:01 +0900

    index 예제

--- IndexerPrac/Program.cs
+++ IndexerPrac/Program.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace IndexerPrac
 {
+    class Products
+    {
+        public int this[int i]
+        {
+            get { return i; }
+            set { Console.WriteLine(i); }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-         
+            Products p = new Products();
+            Console.WriteLine(p[3]);
         }
     }
 }
